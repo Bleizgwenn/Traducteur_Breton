d03490e2ca6b7a8f7535878acd4b19dbb673cc97
Lucas Briand <97041674+lucasbriandbr@users.noreply.github.com>
2023-08-12 11:29:17 +0200

    Je pousse sur github desktop le traducteur

--- komz.bzh/pages/index.tsx
+++ komz.bzh/pages/index.tsx
@@ -1,118 +1,111 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [lang, setLang] = useState(true)
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col items-center bg-black'>
+      <div className='flex flex-col max-w-[1148px] w-full gap-4'>
+
+        <div className='flex flex-row px-4 py-2.5'>
+          <p className='police1 text-5xl tracking-wide font-semibold'>treiñ</p>
         </div>
+
+        <section className='flex flex-col gap-4'>
+
+          <div className='flex flex-row gap-4 text-xs overlow-hidden overflow-x-scroll container-snap'>
+
+            <div className='flex flex-col bg-gray-900 w-fit px-4 py-2.5 rounded-lg whitespace-nowrap	'>
+              <p>Testenn</p>
+            </div>
+
+            <div className='flex flex-col bg-gray-900 w-fit px-4 py-2.5 rounded-lg whitespace-nowrap	'>
+              <p>Skeudenn</p>
+            </div>
+
+            <div className='flex flex-col bg-gray-900 w-fit px-4 py-2.5 rounded-lg whitespace-nowrap	'>
+              <p>Teul</p>
+            </div>
+
+            <div className='flex flex-col bg-gray-900 w-fit px-4 py-2.5 rounded-lg whitespace-nowrap	'>
+              <p>Lec'hienn Genrouedad</p>
+            </div>
+
+          </div>
+
+          <div className='flex flex-col rounded-lg bg-gray-900 w-full h-[300px] border border-gray-700'>
+
+            <div className='flex flex-row items-stretch justify-between border-b border-gray-700 overflow-hidden'>
+
+              <button className={`flex flex-col rounded-lg w-full items-center justify-start`} onClick={()=>{setLang(!lang)}}>
+                
+                <div className={`flex flex-col items-center justify-start max-h-[56px] transition-all duration-300 ease-in-out ${lang?'translate-y-0':'-translate-y-[56px]'}`}>
+
+                  <p className='h-[56px] flex p-4'>Brezhoneg</p>
+
+                  <p className='h-[56px] flex p-4'>Français</p>
+
+                </div>
+
+              </button>
+
+              <button className={`flex flex-col p-4 rounded-full items-center justify-center transition-all duration-300 ease-in-out ${lang?'rotate-180':''}`} onClick={()=>{setLang(!lang)}}>
+
+                <svg fill="white" className="h-4 w-4" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"/>
+                </svg>
+                
+              </button>
+
+              <button className={`flex flex-col rounded-lg w-full items-center justify-end`} onClick={()=>{setLang(!lang)}}>
+                
+                <div className={`flex flex-col justify-end items-center max-h-[56px] transition-all duration-300 ease-in-out ${lang?'translate-y-0':'translate-y-[56px]'}`}>
+
+                  <p className='h-[56px] flex p-4'>Breton</p>
+
+                  <p className='h-[56px] flex p-4'>Galleg</p>
+
+                </div>
+                
+              </button>
+
+            </div>
+
+            <div>
+
+              <div>
+
+              </div>
+
+              <div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </section>
+
+        <section className='flex flex-col rounded-lg bg-gray-900'>
+
+          <button className={`flex flex-col rounded-lg w-full items-start justify-start overflow-hidden`} onClick={()=>{setLang(!lang)}}>
+            
+            <div className={`flex flex-col items-center justify-start max-h-[56px] transition-all duration-300 ease-in-out ${lang?'translate-y-0':'-translate-y-[56px]'}`}>
+
+              <p className='h-[56px] flex p-4'>Geriadur</p>
+
+              <p className='h-[56px] flex p-4'>Dictionnaire</p>
+
+            </div>
+
+          </button>
+
+        </section>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
