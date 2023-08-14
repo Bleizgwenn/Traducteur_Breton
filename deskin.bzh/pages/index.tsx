28f015d8bac37390b974569d36eea3de6168162f
Lucas Briand <97041674+lucasbriandbr@users.noreply.github.com>
2023-08-14 16:32:12 +0200

    Ajout de la page logine pour différencier les deux pages

--- deskin.bzh/pages/index.tsx
+++ deskin.bzh/pages/index.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import { useState, useRef, useEffect } from 'react'
 import Loader from '@/components/Loader'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+
+  const router = useRouter()
 
   const [lang, setLang] = useState(true)
   const [page, setPage] = useState(0)
@@ -24,67 +27,9 @@ export default function Home() {
 
         <section className='flex flex-col rounded-lg bg-gray-900 w-full p-4 gap-4'>
           
-          <p className='police2 text-xl'>Inscription</p>
-          
-          <div className='flex flex-row'>
-          
-            <div className='flex flex-row items-center bg-black/50 rounded-l-lg px-4'>
+          <p className='police2 text-xl'>Vous n'êtes pas connecté</p>
 
-              <input type='text' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='Nom'/>
-
-            </div>
-            
-            <div className='flex flex-row items-center bg-black/50 rounded-r-lg px-4'>
-
-              <input type='text' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='Prénom'/>
-
-            </div>
-
-          </div>
-          
-          <div className='flex flex-row items-center bg-black/50 rounded-lg px-4'>
-            
-            <p>@</p>
-
-            <input type='text' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='pseudonyme'/>
-
-          </div>
-          
-          <div className='flex flex-row items-center bg-black/50 rounded-lg px-4'>
-
-            <input type='password' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='Mot de passe'/>
-
-          </div>
-          
-          <div className='flex flex-row items-center bg-black/50 rounded-lg px-4'>
-
-            <input type='password' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='Confirmez le mot de passe'/>
-
-          </div>
-
-          <button className='flex flex-row bg-black/50 rounded-lg px-4 py-2.5 police2 text-sm items-baseline justify-center'>Valider</button>
-
-        </section>
-
-        <section className='flex flex-col rounded-lg bg-gray-900 w-full p-4 gap-4'>
-          
-          <p className='police2 text-xl'>Connexion</p>
-          
-          <div className='flex flex-row items-center bg-black/50 rounded-lg px-4'>
-            
-            <p>@</p>
-
-            <input type='text' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='pseudonyme'/>
-
-          </div>
-          
-          <div className='flex flex-row items-center bg-black/50 rounded-lg px-4'>
-
-            <input type='password' className='flex flex-row bg-transparent focus:outline-none py-2.5 w-full' placeholder='Mot de passe'/>
-
-          </div>
-
-          <button className='flex flex-row bg-black/50 rounded-lg px-4 py-2.5 police2 text-sm items-baseline justify-center'>Valider</button>
+          <button className='flex flex-row bg-black/50 rounded-lg px-4 py-2.5 police2 text-sm items-baseline justify-center' onClick={()=>{router.push('/login?q=inscription')}}>Connexion</button>
 
         </section>
 
